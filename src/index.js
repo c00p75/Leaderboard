ab5a30f0d2472e5b99f67acc99d20d2b315c01df
c00p75_ <georgecoopmsapenda@gmail.com>
2022-12-22 17:57:14 +0200

    Fetch API data and display it in DOM

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
 import _ from 'lodash'; // eslint-disable-line
 import './style.css';
+
+const boardElement = document.querySelector('#board');
+
+const fetchAPIData = () => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mkTCYEA17yEVtm11XsOo/scores/', {})
+    .then(response => response.json())
+    .then(json => {
+      json.result.forEach((i) => {
+        const item = document.createElement('li');
+        item.innerHTML = `<span>${i.user}</span> : <span>${i.score}</span>`;
+        boardElement.appendChild(item);
+      });
+    });
+};
+
+const postAPIData = (userName, userScore) => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mkTCYEA17yEVtm11XsOo/scores/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      user: userName,
+      score: userScore,
+    }),
+  })
+    .then(response => response.json())
+    .then(json => console.log(json));
+};
+
+// postAPIData('cheez', 4);
+fetchAPIData();
