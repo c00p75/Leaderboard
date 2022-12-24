d6e549ce89beddd677965f453d0ca783846ed751
c00p75_ <georgecoopmsapenda@gmail.com>
2022-12-24 20:06:10 +0200

    Use table for scoreboard

--- src/Modules/Get data.js
+++ src/Modules/Get data.js
@@ -1,14 +1,15 @@
 export default async () => {
-  const list = document.querySelector('#board ul');
-  const scores = document.createElement('ul');
+  const list = document.querySelector('#board table');
+  const scores = document.createElement('table');
+  scores.innerHTML = '<thead><th>User Name</th><th></th><th>Points</th></thead>';
   await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mkTCYEA17yEVtm11XsOo/scores/')
     .then((response) => response.json())
     .then((json) => {
       json.result.sort((a, b) => b.score - a.score);
       json.result.forEach((i) => {
-        const item = document.createElement('li');
-        item.innerHTML = `<span>${i.user}</span> : <span>${i.score}</span>`;
-        scores.appendChild(item);
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${i.user}</td> <td>:</td> <td>${i.score}</td>`;
+        scores.appendChild(row);
       });
     });
   document.querySelector('#board').replaceChild(scores, list);
